1d3ac8d11c1d573fb4b38478884361ce9b1ebc7c
kimsh713 <kim11221122aa@gmail.com>
2021-08-27 18:31:03 +0900

    프나펑 93%

--- Assets/MiniGame/FNK/Scripts/FNK_GameManager.cs
+++ Assets/MiniGame/FNK/Scripts/FNK_GameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class FNK_GameManager : MonoBehaviour
 {
     public List<GameObject> NoteList = new List<GameObject>();
+    public List<GameObject> EffectList = new List<GameObject>();
 
     [SerializeField] Transform Center = null;
     [SerializeField] Transform Up = null;
@@ -46,36 +47,34 @@ public class FNK_GameManager : MonoBehaviour
             hp = maxhp;
         Hp.fillAmount = Mathf.Lerp(Hp.fillAmount, hp / maxhp, Time.deltaTime * 5);
 
-        if (GetComponent<Note_Manager>().SpawnEnd == true)
-        {
-            EndTimer += Time.deltaTime;
-            if (EndTimer > 3)
-            {
-                Clear.SetActive(true);
-                FindObjectOfType<FlameScript>().Music.Stop();
-                GetComponent<Note_Manager>().SongEnd = true;
-                LastScore.gameObject.SetActive(true);
-                LastScore.text = score.ToString();
-                if(Input.anyKeyDown)
-                {
-                    SceneManager.LoadScene(0);
-                    EndTimer = 0;
-                }
-            }
-        }
+        //if (GetComponent<Note_Manager>().SpawnEnd == true)
+        //{
+        //    EndTimer += Time.deltaTime;
+        //    if (EndTimer > 3)
+        //    {
+        //        Clear.SetActive(true);
+        //        FindObjectOfType<FlameScript>().Music.Stop();
+        //        GetComponent<Note_Manager>().SongEnd = true;
+        //        LastScore.gameObject.SetActive(true);
+        //        if (Input.anyKeyDown)
+        //        {
+        //            SceneManager.LoadScene(0);
+        //            EndTimer = 0;
+        //        }
+        //    }
+        //}
 
-        if (hp <= 0)
-        {
-            Over.SetActive(true);
-            FindObjectOfType<FlameScript>().Music.Stop();
-            GetComponent<Note_Manager>().SongEnd = true;
-            LastScore.gameObject.SetActive(true);
-            LastScore.text = score.ToString();
-            if (Input.anyKeyDown)
-            {
-                SceneManager.LoadScene(0);
-            }
-        }
+        //if (hp <= 0)
+        //{
+        //    Over.SetActive(true);
+        //    FindObjectOfType<FlameScript>().Music.Stop();
+        //    GetComponent<Note_Manager>().SongEnd = true;
+        //    LastScore.gameObject.SetActive(true);
+        //    if (Input.anyKeyDown)
+        //    {
+        //        SceneManager.LoadScene(0);
+        //    }
+        //}
     }
 
     public void CheakTiming(int type)
@@ -94,6 +93,7 @@ public class FNK_GameManager : MonoBehaviour
                         case 0:
                             if (NotePosX == Up.position.x)
                             {
+                                Instantiate(EffectList[0], NoteList[i].transform.parent);
                                 Destroy(NoteList[i]);
                                 NoteList.RemoveAt(i);
                                 switch (y)
@@ -101,22 +101,22 @@ public class FNK_GameManager : MonoBehaviour
                                     case 0:
                                         combo++;
                                         hp += 5;
-                                        StartCoroutine(AddScore(score + 300, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 300, score));
                                         break;
                                     case 1:
                                         combo++;
                                         hp += 3;
-                                        StartCoroutine(AddScore(score + 200, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 200, score));
                                         break;
                                     case 2:
                                         combo++;
                                         hp += 2;
-                                        StartCoroutine(AddScore(score + 100, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 100, score));
                                         break;
                                     case 3:
                                         combo++;
                                         hp += 1;
-                                        StartCoroutine(AddScore(score + 50, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 50, score));
                                         break;
                                 }
                                 return;
@@ -125,6 +125,7 @@ public class FNK_GameManager : MonoBehaviour
                         case 1:
                             if (NotePosX == Down.position.x)
                             {
+                                Instantiate(EffectList[1], NoteList[i].transform.parent);
                                 Destroy(NoteList[i]);
                                 NoteList.RemoveAt(i);
                                 switch (y)
@@ -132,22 +133,22 @@ public class FNK_GameManager : MonoBehaviour
                                     case 0:
                                         combo++;
                                         hp += 5;
-                                        StartCoroutine(AddScore(score + 300, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 300, score));
                                         break;
                                     case 1:
                                         combo++;
                                         hp += 3;
-                                        StartCoroutine(AddScore(score + 200, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 200, score));
                                         break;
                                     case 2:
                                         combo++;
                                         hp += 2;
-                                        StartCoroutine(AddScore(score + 100, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 100, score));
                                         break;
                                     case 3:
                                         combo++;
                                         hp += 1;
-                                        StartCoroutine(AddScore(score + 50, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 50, score));
                                         break;
                                 }
                                 return;
@@ -156,6 +157,7 @@ public class FNK_GameManager : MonoBehaviour
                         case 2:
                             if (NotePosX == Left.position.x)
                             {
+                                Instantiate(EffectList[2], NoteList[i].transform.parent);
                                 Destroy(NoteList[i]);
                                 NoteList.RemoveAt(i);
                                 switch (y)
@@ -163,22 +165,22 @@ public class FNK_GameManager : MonoBehaviour
                                     case 0:
                                         combo++;
                                         hp += 5;
-                                        StartCoroutine(AddScore(score + 300, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 300, score));
                                         break;
                                     case 1:
                                         combo++;
                                         hp += 3;
-                                        StartCoroutine(AddScore(score + 200, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 200, score));
                                         break;
                                     case 2:
                                         combo++;
                                         hp += 2;
-                                        StartCoroutine(AddScore(score + 100, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 100, score));
                                         break;
                                     case 3:
                                         combo++;
                                         hp += 1;
-                                        StartCoroutine(AddScore(score + 50, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 50, score));
                                         break;
                                 }
                                 return;
@@ -187,6 +189,7 @@ public class FNK_GameManager : MonoBehaviour
                         case 3:
                             if (NotePosX == Right.position.x)
                             {
+                                Instantiate(EffectList[3], NoteList[i].transform.parent);
                                 Destroy(NoteList[i]);
                                 NoteList.RemoveAt(i);
                                 switch (y)
@@ -194,22 +197,22 @@ public class FNK_GameManager : MonoBehaviour
                                     case 0:
                                         combo++;
                                         hp += 5;
-                                        StartCoroutine(AddScore(score + 300, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 300, score));
                                         break;
                                     case 1:
                                         combo++;
                                         hp += 3;
-                                        StartCoroutine(AddScore(score + 200, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 200, score));
                                         break;
                                     case 2:
                                         combo++;
                                         hp += 2;
-                                        StartCoroutine(AddScore(score + 100, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 100, score));
                                         break;
                                     case 3:
                                         combo++;
                                         hp += 1;
-                                        StartCoroutine(AddScore(score + 50, score));
+                                        StartCoroutine(AddScore(score + (combo * 3) + 50, score));
                                         break;
                                 }
                                 return;
@@ -224,19 +227,20 @@ public class FNK_GameManager : MonoBehaviour
 
     IEnumerator AddScore(float target, float current)
     {
-        float duration = 0.4f;
+        float duration = 0.35f;
         float offset = (target - current) / duration;
 
         score += (target - score) + (combo * 3);
 
-        while (current < target)
+        while (current <= target)
         {
             current += offset * Time.deltaTime;
             Score.text = ((int)current).ToString();
             yield return null;
         }
         current = target;
-        Score.text = ((int)current).ToString();
+        Score.text = score.ToString();
+        LastScore.text = score.ToString();
     }
 }
 
